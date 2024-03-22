837a86665570f74f232c9561a2920d65f0a2361d
Alfred Wallace <contact@alfredwallace.me>
2024-03-21 19:10:30 -0700

    Fixed save data

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,49 +1,50 @@
-
-//guide.addEventListener("click", function() {
-//    window.location.replace("./Project-LANTA/guide.html");
-//});
-
-// add return button event?
-// add reset button function + event
-
 $(document).ready(function () {
     // TODO: make all important variables
     var pizzaSliceCount = 0;
     var catCount = 0;
     var priceToHireACat = 10;
     var weatherID = $("#api-cat")
-    // TODO: make Reset button event listener
-    $("#resetBtn").click(function () {
-        var newSave = {
-            "pizzas": 0,
-            "cat count": 0,
-        }
-        console.log(newSave)
-        localStorage.setItem("defaultSave", JSON.stringify(newSave))
-    })
 
-    $("#saveBtn").click(function () {
+    // Helper function that 
+    function SaveData(newPizzaCount = 0, newCatCount = 0, newCatPrice = 10) {
+        // Make new save data
         var newSave = {
-            "pizzas": pizzaSliceCount,
-            "cat count": catCount,
+            "pizzas": newPizzaCount,
+            "cat_count": newCatCount,
+            "hire_a_cat_price": newCatPrice
         }
         console.log(newSave)
         localStorage.setItem("defaultSave", JSON.stringify(newSave))
-        // defaultSave = pizzaSliceCount
-        // catsHiredCount
-    })
-    // TODO: Check for saved data
+
+        pizzaSliceCount = newPizzaCount;
+        $("#pizzaSliceCount").text(pizzaSliceCount);
+        catCount = newCatCount;
+        $("#catCount").text(catCount);
+        priceToHireACat = newCatPrice;
+        $("#hireACatCost").text(priceToHireACat);
+    }
+
+
+    // Check for saved data
     var saveData = JSON.parse(localStorage.getItem("defaultSave"));
     console.log("saveData: ");
     console.log(saveData);
-    // TODO: If none, create empty save data
+
+    // If none, create empty save data
     if (saveData == null) {
         console.log("empty!")
         // Make new save data
-        $("#hireACat").hide();
-        $("#catCountLabel").hide();
-    } $("#pizzaSliceCount").text(saveData["pizzas"])
-    $("#catCount").text(saveData["cat count"])
+        SaveData();
+    }
+
+    $("#saveBtn").click(function () {
+        SaveData(pizzaSliceCount, catCount, priceToHireACat);
+    })
+
+    // TODO: make Reset button event listener
+    $("#resetBtn").click(function () {
+        SaveData();
+    })
 
     // TODO: Create pizza button listener
     $("#pizzaButton").click(function () {
@@ -53,6 +54,8 @@ $(document).ready(function () {
             $("#hireACat").show();
         }
     });
+
+
 
     // TODO: Create hire cat button listener
     // Don't hire a cat if you can't afford one
@@ -79,25 +82,26 @@ $(document).ready(function () {
     const APIkey = "43d3ce9a4d6be02e5f3dbc9ba49a17b0"
     var apiURL = 'http://api.openweathermap.org/geo/1.0/direct?q={cityName}&limit=5&units=imperial&appid=' + APIkey;
     var weatherAPI = "https://api.openweathermap.org/data/2.5/weather?lat={lat}&lon={lon}&units=imperial&appid=" + APIkey;
-    
+
     function showWeather(weatherID) {
-        var validURL = apiURL.replace("{cityName}",weatherID);
-    
+        var validURL = apiURL.replace("{cityName}", weatherID);
+
         $.ajax({
-        url: validURL,
-        method: 'GET',
-        success: function (response) {
-            // Handle the API response here
-            console.log(response);
-            var json = JSON.parse(JSON.stringify(response));
-            getWeather(json[0].lat, json[0].lon)
-        },
-        error: function (xhr, status, error) {
-            // Handle errors here
-            // console.error(status, error);
-        }})
+            url: validURL,
+            method: 'GET',
+            success: function (response) {
+                // Handle the API response here
+                console.log(response);
+                var json = JSON.parse(JSON.stringify(response));
+                getWeather(json[0].lat, json[0].lon)
+            },
+            error: function (xhr, status, error) {
+                // Handle errors here
+                // console.error(status, error);
+            }
+        })
     }
-    function getWeather (lat, lon) {
+    function getWeather(lat, lon) {
         var currentAPI = "https://api.openweathermap.org/data/2.5/weather?lat={lat}&lon={lon}&units=imperial&appid=" + APIkey;
         var currentURL = currentAPI.replace("{lat}", lat);
         var currentURL = currentURL.replace("{lon}", lon);
@@ -119,4 +123,4 @@ $(document).ready(function () {
 //Every 1 second, pizza slices should increase based on # of cats (Adjoa)
 //Every 1 minute (arbitrarily longer amount of time), check the value of [Pizza Company] stocks, print that number somewhere beneath the Pizza button
 //Every 1 minute (arbitrarily longer amount of time) check the weather in Italy or something, print the current temperature somewhere beneath the Pizza button
-//Reset button should create a modal that asks if you're REALLY sure you wanna lose your save data (pizza count and cat count)
+//Reset button should create a modal that asks if you're REALLY sure you wanna lose your save data (pizza count and cat-count)
